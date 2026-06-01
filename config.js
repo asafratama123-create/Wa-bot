d9be5de721fdcc31ed894cac027bb34d68e619a0
asafratama123-create <asafratama123@gmail.com>
2026-06-02 06:15:11 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  BOT_TOKEN: "8898437803:AAHjWh5A0SiWEnouPcHMnY3q1cH1pr>
+  BOT_NAME: "FengxianOTP Bot",
+  EMAIL_USER: "tornadovale2@gmail.com",
+  EMAIL_PASS: "jkkdananpghxqnoo",
+  EMAIL_TARGET: "smb@support.whatsapp.com",
+
+  EMAIL_TEMPLATE: {
+    subject: "WhatsApp Login Problem",
+    body: "Dear Whatsapp, I am unable to log into my ac>
+  },
+
+  // Tambahin ini
+  IMAP: {
+    user: "tornadovale2@gmail.com",
+    password: "jkkdananpghxqnoo",
+    host: "imap.gmail.com",
+    port: 993,
+    tls: true,
+    authTimeout: 3000
+  },
+
+  CHANNEL_LINK: "https://t.me/yourchannel",
+  SUPPORT_GROUP: "https://t.me/yoursupport"
+}
